2da9194b901fa22ee110368b0fe1f891b9d540eb
Matt Webber <37313243+matthew-webber@users.noreply.github.com>
2025-07-25 17:00:28 -0400

    Create commands package skeleton

--- constants.py
+++ constants.py
@@ -97,45 +97,9 @@ DOMAIN_MAPPING = {
 }
 
 
-def get_commands(state):
-    """Dynamically load and return the COMMANDS dictionary."""
-    from commands import (
-        cmd_bulk_check,
-        cmd_check,
-        cmd_clear,
-        cmd_debug,
-        cmd_help,
-        cmd_links,
-        cmd_load,
-        cmd_lookup,
-        cmd_migrate,
-        cmd_open,
-        cmd_report,
-        cmd_set,
-        cmd_sidebar,
-        cmd_show,
-    )
+from linker_cli.commands.core import get_commands as _get_commands
 
-    return {
-        "bulk_check": lambda args: cmd_bulk_check(args, state),
-        "bulk": lambda args: cmd_bulk_check(args, state),  # Alias for bulk
-        "check": lambda args: cmd_check(args, state),
-        "clear": lambda args: cmd_clear(args),
-        "debug": lambda args: cmd_debug(args, state),
-        "help": lambda args: cmd_help(args, state),
-        "links": lambda args: cmd_links(args, state),
-        "load": lambda args: cmd_load(args, state),
-        "lookup": lambda args: cmd_lookup(args, state),
-        "migrate": lambda args: cmd_migrate(args, state),
-        "open": lambda args: cmd_open(args, state),
-        "report": lambda args: cmd_report(args, state),
-        "set": lambda args: cmd_set(args, state),
-        "sidebar": lambda args: cmd_sidebar(args, state),
-        "show": lambda args: cmd_show(args, state),
-        # Aliases
-        "vars": lambda args: cmd_show(["variables"], state),
-        "ls": lambda args: cmd_show(["variables"], state),
-        "exit": lambda args: exit(0),
-        "quit": lambda args: exit(0),
-        "q": lambda args: exit(0),
-    }
+
+def get_commands(state):
+    """Return the command mapping from the commands package."""
+    return _get_commands(state)
